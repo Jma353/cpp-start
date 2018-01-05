f243150ded5512c7de436fe79b2a44292aaf615c
Jma353 <jma353@cornell.edu>
2018-01-05 14:16:42 -0500

    Strings

---
+++ c1/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // Leveraging C++ string
+  std::string hello = "Hello ";
+  std::string world = "world!";
+  // Show the output
+  std::cout << hello + world << std::endl;
+  return 0;
+}
